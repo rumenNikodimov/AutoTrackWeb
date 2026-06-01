c7da255ab303a64c3f97d86a3a4601a9fb77131e
Rumen Nikodimov <Rumen.Nikodimov@ict.eu>
2026-06-01 13:07:14 +0300

    small change

--- autotrack-web-vite/src/services/api.ts
+++ autotrack-web-vite/src/services/api.ts
@@ -8,7 +8,7 @@
 // const API_BASE_URL =
 //   import.meta.env.VITE_API_URL ?? "https://autotrack-api.onrender.com";
 
-const API_BASE_URL = "https://autotrackapi1.onrender.com";
+const API_BASE_URL = "https://autotrackapi1.onrender.com/api";
 
 
 
